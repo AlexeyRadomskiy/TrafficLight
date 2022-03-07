a6a18c36abc0ecb924e5c670a4f8f9bc4b795406
Alexey <alexeyradomskiy98@gmail.com>
2022-03-07 15:44:34 +0300

    Setup ViewController.swift

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -7,13 +7,63 @@
 
 import UIKit
 
+enum CurrentLight {
+    case red, yellow, green
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redLightView: UIView!
+    @IBOutlet weak var yellowLightView: UIView!
+    @IBOutlet weak var greenLightView: UIView!
+    
+    @IBOutlet weak var changingLightButton: UIButton!
+    
+    private var currentLight = CurrentLight.red
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLightView.backgroundColor = .systemRed
+        redLightView.layer.cornerRadius = 60
+        redLightView.alpha = 0.3
+        
+        yellowLightView.backgroundColor = .systemYellow
+        yellowLightView.layer.cornerRadius = 60
+        yellowLightView.alpha = 0.3
+        
+        greenLightView.backgroundColor = .systemGreen
+        greenLightView.layer.cornerRadius = 60
+        greenLightView.alpha = 0.3
+        
+        changingLightButton.backgroundColor = .secondarySystemBackground
+        changingLightButton.tintColor = .gray
+        changingLightButton.layer.cornerRadius = 15
+        changingLightButton.setTitle("START", for: .normal)
+        changingLightButton.setTitleColor(.white, for: .normal)
+
     }
 
-
+    @IBAction func changingLightButtonTapped() {
+        if changingLightButton.currentTitle == "START" {
+            changingLightButton.setTitle("NEXT", for: .normal)
+        }
+        
+        switch currentLight {
+        case .red:
+            greenLightView.alpha = 0.3
+            redLightView.alpha = 1.0
+            currentLight = .yellow
+        case .yellow:
+            redLightView.alpha = 0.3
+            yellowLightView.alpha = 1.0
+            currentLight = .green
+        case .green:
+            yellowLightView.alpha = 0.3
+            greenLightView.alpha = 1.0
+            currentLight = .red
+        }
+    }
+    
 }
 
